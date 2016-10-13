f2d291792bdd5bb3e4103d069632cf1d8b7451af
Akshit Gupta <akshitonline@gmail.com>
2016-10-13 08:46:24 +0530

    minor changes and adjustments

--- app/src/main/java/com/android/akshitgupta/capstoneproject/SimpleWidgetProvider.java
+++ app/src/main/java/com/android/akshitgupta/capstoneproject/SimpleWidgetProvider.java
@@ -12,10 +12,10 @@ import android.widget.RemoteViews;
 
 import com.android.akshitgupta.capstoneproject.dailyprediction.DailyPredictionActivity;
 import com.android.akshitgupta.capstoneproject.data.UserContract;
-import com.android.akshitgupta.capstoneproject.data.UserProvider;
+import com.android.akshitgupta.capstoneproject.enums.Gender;
 import com.android.akshitgupta.capstoneproject.object.User;
-
-import java.util.Random;
+import com.android.akshitgupta.capstoneproject.utils.ConstantUtils;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by akshitgupta on 12/10/16.
@@ -24,41 +24,48 @@ import java.util.Random;
 public class SimpleWidgetProvider extends AppWidgetProvider {
 
     private User userProfile;
+    SharedPreferences prefs;
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int count = appWidgetIds.length;
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         for (int i = 0; i < count; i++) {
             int widgetId = appWidgetIds[i];
-            String number = String.format("%03d", (new Random().nextInt(900) + 100));
 
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
                     R.layout.simple_widget);
-            remoteViews.setTextViewText(R.id.textView, number);
+            String defaultUserId = prefs.getString("userDefaultId", ConstantUtils.DEFAULT_WIDGET_USER_ID);
+            if (ConstantUtils.DEFAULT_WIDGET_USER_ID.equals(defaultUserId)) {
+                remoteViews.setTextViewText(R.id.widget_msg, context.getString(R.string.widget_error_msg));
+                remoteViews.setBoolean(R.id.widget_action, "setEnabled", false);
 
-            updateUserProfile(context);
-            // Create intent to launch MainActivity
-            Intent intent1 = new Intent(context, DailyPredictionActivity.class);
-            intent1.putExtra("userProfile", userProfile);
-            PendingIntent pendingIntent1 = PendingIntent.getActivity(context, 0, intent1, 0);
-            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent1);
+            } else {
+                updateUserProfile(context, defaultUserId);
+                Intent intent = new Intent(context, DailyPredictionActivity.class);
+                intent.putExtra("userProfile", userProfile);
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+                remoteViews.setOnClickPendingIntent(R.id.widget_action, pendingIntent);
+                remoteViews.setTextViewText(R.id.simple_widget_user_name, userProfile.getUserName());
+                remoteViews.setTextViewText(R.id.simple_widget_user_dob, userProfile.getDobDate()+" "+userProfile.getDobTIme());
+                remoteViews.setTextViewText(R.id.simple_widget_user_city, userProfile.getCityName());
+                if (Gender.MALE.getCode().equals(userProfile.getUserGender())) {
+                    remoteViews.setImageViewResource(R.id.simple_widget_image,R.drawable.male_default);
+                } else {
+                    remoteViews.setImageViewResource(R.id.simple_widget_image,R.drawable.female_default);
 
-           /* Intent intent = new Intent(context, SimpleWidgetProvider.class);
-            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
-           */
+                }
 
+
+            }
 
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
-    public void updateUserProfile(Context context) {
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        String defaultUserId = prefs.getString("userDefaultId", "1");
+    public void updateUserProfile(Context context, String defaultUserId) {
+
 
         Cursor userCursor = context.getContentResolver().query(
                 UserContract.UserEntry.CONTENT_URI,
@@ -80,5 +87,5 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
         }
         userCursor.close();
     }
-    }
+}
 
